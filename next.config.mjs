bb8655bd56f6f7c8a9a8205fda1c99c7bff75f6e
Adarsh Dhote <adarsh@Adarshs-MacBook-Air.local>
2025-07-18 18:05:00 +0530

    latest

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: isProd ? "/qunova-task" : "",
+  trailingSlash: true,
+};
 
 export default nextConfig;
